8ba3c30ca981ab8188708bfd297d5dee8e2f744f
Piotr Zientara <314zientara@gmail.com>
2018-01-31 23:22:18 +0100

    fix eslint errors and tests

---
+++ src/App.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import App from './App';
+import reducers from './reducers';
+
+/* eslint-disable no-underscore-dangle */
+const store = createStore(
+  reducers, /* preloadedState, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk),
+);
+/* eslint-enable */
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div,
+  );
+  ReactDOM.unmountComponentAtNode(div);
+});
